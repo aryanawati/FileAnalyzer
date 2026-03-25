7870790a0f119c56eb934f36e3098152f7c521d7
aryan <aryan.awati@gmail.com>
2026-03-25 00:31:49 -0400

    Playing with it

---
+++ main.py
@@ -0,0 +1,16 @@
+import time
+
+closingMessage = "\nFinished writing! Closing now..."
+
+x = 1
+fi = open("outputfile.txt" , "a")
+for i in range(15):
+    fi.write(f"{str(x)}\n")
+    x += 1
+
+for i in range(10, 0, -1):
+    fi.write(f"Deleting in {i} seconds.\n")
+    time.sleep(1)
+    fi.flush()
+
+open("outputfile.txt" , "w").close()
