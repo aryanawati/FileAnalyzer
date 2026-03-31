9200a0b6f783229cfb32bcd033839286b03ef615
aryan <aryan.awati@gmail.com>
2026-03-31 16:34:44 -0400

    Actually reads the file now, returns lines, words, chars, and chars(no spaces)

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
-import time
+def countWords(lineList):
+    words = 0
+    chars = 0
+    spaces = 0
+    for line in lineList:
+        for letter in line:
+            if letter == " " or letter == "\n" or letter == "\t":
+                spaces += 1
+            else:
+                chars += 1
+        wordlist = line.split()
+        words += len(wordlist)
+    return (words, chars, spaces)
 
-closingMessage = "\nFinished writing! Closing now..."
 
-x = 1
-fi = open("outputfile.txt" , "a")
-for i in range(15):
-    fi.write(f"{str(x)}\n")
-    x += 1
+inputFile = open("inputfile.txt" , "r")
+outputFile = open("outputfile.txt" , "w")
 
-for i in range(10, 0, -1):
-    fi.write(f"Deleting in {i} seconds.\n")
+contents = inputFile.readlines()
+lines = len(contents)
+wordCount, charCount, spaceCount = countWords(contents)
 
-fi.write(closingMessage)
-fi.close()
+outputFile.write(f"Number of Lines: {lines}\n")
+outputFile.write(f"Number of Words: {wordCount}\n")
+outputFile.write(f"Number of Characters: {charCount}\n")
+outputFile.write(f"Number of Characters (No Spaces): {charCount - spaceCount}\n")
+
+outputFile.close()
+inputFile.close()
